a8fdc8583bf046fcc04fcd854674d60aacd2a857
minhanPark <alsgks1218@naver.com>
2023-12-07 17:58:15 +0900

    .

--- src/app/(after-login)/home/page.tsx
+++ src/app/(after-login)/home/page.tsx
@@ -1,3 +1,28 @@
-export default function Page() {
-  return <div className="">페이지</div>;
+import style from "./home.module.css";
+import Tab from "@/app/(after-login)/home/_component/Tab";
+import TabProvider from "@/app/(after-login)/home/_component/TabProvider";
+import PostForm from "@/app/(after-login)/home/_component/PostForm";
+import Post from "@/app/(after-login)/_component/Post";
+
+export default function Home() {
+  return (
+    <main className={style.main}>
+      <TabProvider>
+        <Tab />
+        <PostForm />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </TabProvider>
+    </main>
+  );
 }
